d0411d253c3198b69ebe901133fd0026ae993f66
Abu Saad As-Siddique <abdulhaleem.mamangcao@gmail.com>
2025-12-25 18:27:40 +0800

    Fixing auth issues

--- src/app/api/strava/refresh/route.ts
+++ src/app/api/strava/refresh/route.ts
@@ -40,6 +40,7 @@ export async function POST(request: Request) {
     const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
